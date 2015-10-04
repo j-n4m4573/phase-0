77b10094631614c00193c736170379fd02731cf5
Jamar Gibbs <jmg220p@gmail.com>
2015-10-04 06:07:32 -0700

    add 4.3.1 and 4.3.2

---
+++ week-4/variables-methods.rb
@@ -0,0 +1,10 @@
+puts "What is your name?"
+name = gets.chomp
+
+puts "What is your middle name"
+middle_name = gets.chomp
+
+puts "What is your last name"
+last_name = gets.chomp
+
+puts "hi" + ' ' + name + ' ' + middle_name + ' ' + last_name  
---
+++ week-4/variables-methods2.rb
@@ -0,0 +1,5 @@
+puts "What is your favorite number?"
+number = gets.chomp
+
+puts number.to_i + 1 
+puts "This is a better number, ok?"
