7c907f65a66c994daec2b830dd5d4653c2dd4454
Jamar Gibbs <jmg220p@gmail.com>
2015-11-02 09:34:00 -0800

    add updates nums

--- week-5/nums-commas/my_solution.rb
+++ week-5/nums-commas/my_solution.rb
@@ -29,21 +29,40 @@ def separate_comma(integer)
 	if  comma.length > 9
 	  	comma.insert(9,',') 
 	end
-	if  comma.length > 13
+	if  comma.length > 12
 	  	comma.insert(13,',') 
 	  else
 	  	p comma
 	end
 end
 
-p separate_comma(2000000000)
-
-
-
-
 
 # 2. Refactored Solution
 
+def separate_comma(integer)
+	comma = integer.to_s
+	if comma.split
+		
+# 3. Reflection
+
+# What was your process for breaking the problem down? What different approaches did you consider?
+
+# My process for breaking the problem down was pseudocoding then trying to go step by step and code out the pieces based on the assumption made. 
+
+# I considered several different processes but unfortunately my limited time and knowledge got in the way and I had to go with the very first approach that came to my mind.
+
+# Was your pseudocode effective in helping you build a successful initial solution?
+
+# Yes my pseudocode was effective in helping me to build a successful initial solution. It helped me to decide which steps I should take next and kept me focused on the input and output of the 
+# solution when I got stuck. 
+
+# What Ruby method(s) did you use when refactoring your solution? What difficulties did you have implementing it/them? Did it/they significantly change the way your code works? If so, how?
+# How did you initially iterate through the data structure?
+
+# Initially I iterated through the data structure using conditional statements. Was working on a more concise solution but ran out of time and had to write this reflection instead.
+
+# Do you feel your refactored solution is more readable than your initial solution? Why?
+# My refactored solution could have be far more concise, efficient and readable its just I didnt have enough time to actually dig as deep as I wanted to. 
 
 
 
