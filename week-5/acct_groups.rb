5d95cced828a77071368d3fc403f8967e25e248b
Jamar Gibbs <jmg220p@gmail.com>
2015-11-02 01:22:30 -0800

    add 5.6

--- week-5/acct_groups.rb
+++ week-5/acct_groups.rb
@@ -9,15 +9,43 @@
 # output : groups containing 4 to 5 but no less than 3
 
 # first step calculate total number of people
-
-# see if divisible by 3 
-# see if divisible by 4
-# see if divisible by 5
+# 60 students
 
 # calculate number of groups
-
+# see if divisible by 3 
+# 60/3 == 20
+# see if divisible by 4
+# 60/4 == 15
+# see if divisible by 5
+# 60/5 == 12
 # create number of groups 
-
+# optimal number of groups is 5, the larger the better
 # sort list evenly into groups
-
+# create hash and assign students to groups
 # print list of groups and names
+
+def account_groups(student_names)
+	if student_names % 5 == 0
+		 groups = [group_1, group_1, group_2, group_3, 
+		 			group_4,group_5, group_6, group_7, group_8,
+		 			group_9, group_10, group_11, group_12]
+
+		group_1 << student_names[0..4]
+		group_2 << student_names[5..9]
+		group_3 << student_names[10..14]
+		group_4 << student_names[15..19]
+		group_5 << student_names[20..24]
+		group_6 << student_names[25..29]
+		group_7 << student_names[30..34]
+		group_8 << student_names[35..39]
+		group_9 << student_names[40..44]
+		group_10 << student_names[45..49]
+		group_11 << student_names[50..54]
+		group_12 << student_names[55..60]
+	end 
+		p groups 
+	end
+
+	
+
+
